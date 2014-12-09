efea2894d0476314ea1ab4334850becfe92f1048
MendezV <som.felipe66@gmail.com>
2014-12-09 13:44:40 -0500

    Encuentra el máximo producto de cuatro números consecutivos horizontal, vertical diagonalmente hacia la derecha en el archivo grid.dat

--- 11.py
+++ 11.py
@@ -4,33 +4,45 @@ datos=np.loadtxt("./grid.dat")
 
 
 
-prod_long=1
-prod_ant_diag_1=1
+prod=1
 prod_diag_1=1
 prod_ant_diag_2=1
 prod_diag_2=1
 i_prime=0
 j_prime=0
-
-for i in range(0,len(datos[0,:])-5):
-    for j in range(0,len(datos[:,0])-5):
+pos=0
+i_max=len(datos[:,0])-1
+j_max=len(datos[0,:])-1
+for i in range(0,i_max-3):
+    for j in range(0,j_max-3):
         prod_ant_long_1=1
         prod_ant_long_2=1
+        prod_ant_diag_1=1
         for k in range(0,4):
             prod_ant_long_1=prod_ant_long_1*datos[i,j+k]
             prod_ant_long_2=prod_ant_long_2*datos[i+k,j]
+            prod_ant_diag_1=prod_ant_diag_1*datos[i+k,j+k]
 
-        if(prod_ant_long_1<prod_ant_long_2):
-            if(prod_ant_long_2>prod_long):
-                prod_long=prod_ant_long_2
+        if(prod_ant_long_1<prod_ant_long_2)&(prod_ant_diag_1<prod_ant_long_2):
+            if(prod_ant_long_2>prod):
+                prod=prod_ant_long_2
                 i_prime=i
                 j_prime=j
-        if(prod_ant_long_1>prod_ant_long_2):
-            if(prod_ant_long_1>prod_long):
-                prod_long=prod_ant_long_2
+                pos=datos[i,j]
+        if(prod_ant_long_1>prod_ant_long_2)&(prod_ant_diag_1<prod_ant_long_1):
+            if(prod_ant_long_1>prod):
+                prod=prod_ant_long_1
                 i_prime=i
                 j_prime=j
+                pos=datos[i,j]
+        if(prod_ant_long_1<prod_ant_diag_1)&(prod_ant_diag_1>prod_ant_long_2):
+            if(prod_ant_diag_1>prod):
+                prod=prod_ant_diag_1
+                i_prime=i
+                j_prime=j
+                pos=datos[i,j]
 
-print prod_long
+print prod
 print i_prime
 print j_prime
+print pos
