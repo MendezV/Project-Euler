bc54081fc368dd94ea19135ae748310a9a6f6872
MendezV <som.felipe66@gmail.com>
2014-12-09 14:32:21 -0500

    sirve bien con ambas diagonales , ahora comprimir el código fuente

--- 11.py
+++ 11.py
@@ -5,14 +5,12 @@ datos=np.loadtxt("./grid.dat")
 
 
 prod=1
-prod_diag_1=1
-prod_ant_diag_2=1
-prod_diag_2=1
+
 i_prime=0
 j_prime=0
 pos=0
-i_max=len(datos[:,0])-1
-j_max=len(datos[0,:])-1
+i_max=len(datos[:,0])
+j_max=len(datos[0,:])
 for i in range(0,i_max-3):
     for j in range(0,j_max-3):
         prod_ant_long_1=1
@@ -46,3 +44,23 @@ print prod
 print i_prime
 print j_prime
 print pos
+
+
+prod=1
+
+for i in range(0,i_max-3):
+    for j in range(3,j_max):
+        prod_ant_diag_2=1
+        for k in range(0,4):
+            prod_ant_diag_2=prod_ant_diag_2*datos[i+k,j-k]
+        if(prod_ant_diag_2>prod):
+            prod=prod_ant_diag_2
+            i_prime=i
+            j_prime=j
+            pos=datos[i,j]
+print prod
+print i_prime
+print j_prime
+print pos
+
+
